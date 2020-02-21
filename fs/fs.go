c35bf0d8448981a4c15bb284ad16f98017b3c42f
barjoco <barjo.email@gmail.com>
2020-02-21 22:24:05 +0000

    added FileExists func

--- fs/fs.go
+++ fs/fs.go
@@ -60,3 +60,15 @@ func ReadCSV(path string) ([][]string, error) {
 
 	return records, nil
 }
+
+// FileExists checks if file exists
+func FileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
